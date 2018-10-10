27f3307638d65e61792be2112a7190bbb56032d8
Terry <976680568@qq.com>
2018-10-10 16:46:52 +0800

    Shimming

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-   import _ from 'lodash';
-  
-  function component() {
-    let element = document.createElement('div');
+function component() {
+  let element = document.createElement('div');
 
    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-  }
+  return element;
+}
 
-  document.body.appendChild(component());
+document.body.appendChild(component());
